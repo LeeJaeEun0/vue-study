35d4388d11ef770d4c3b7f61e442e40ede5eaf2e
LeeJaeEun0 <wodmschlrh12@naver.com>
2023-10-30 20:14:51 +0900

    vue for문 사용하기

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <div>
-    원룸샵
-    <h4 class="red" :style="스타일">xx 원룸</h4>
-    <p>{{price1}} 만원</p>
-  </div>
-  <div>
-    <h4>xx 원룸</h4>
-    <p>{{price2}} 만원</p>
-  </div>
-  
-</template>
+  <div class="menu"> 
+      <a v-for="i in menu" :key="i">{{i}}</a>
+    </div>
 
+  <div>
+    <h4 class="red" :style="스타일">{{products[0]}}</h4>
+    <p>100 만원</p>
+    <h4 class="red" :style="스타일">{{products[1]}}</h4>
+    <p>80 만원</p>
+    <h4 class="red" :style="스타일">{{products[2]}}</h4>
+    <p>90 만원</p>
+  </div>
+
+
+</template>
 <script>
 
 
@@ -19,10 +21,9 @@ export default {
   name: 'App',
   data(){
     return {
-      price1 : 60,
-      price2 : 70,
       스타일: "color:blue",
-      products : ['역삼동원룸', '천호동원룸', '마포구원룸']
+      menu : ['Home', 'Shop', 'About'],
+      products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
     }
   },
   components: {
@@ -37,6 +38,15 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.menu{
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a{
+  color: white;
+  padding: 10px;
 }
 </style>
