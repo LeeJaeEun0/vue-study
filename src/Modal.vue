ac8dd87170722b92fb47995b52b5cc9ba590b5f1
LeeJaeEun0 <wodmschlrh12@naver.com>
2023-10-31 20:56:54 +0900

    input 하는 방법

--- src/Modal.vue
+++ src/Modal.vue
@@ -5,6 +5,15 @@
       <h4>{{원룸들[clickNumber].title}}</h4>
       <p>{{원룸들[clickNumber].price}}</p>
       <p>{{원룸들[clickNumber].content}}</p>
+      <!-- <input @input="month=$event.target.value"> -->
+      <input v-model="month">
+      <!-- <input v-model.number="month"> 문자로된 숫자를 숫자로! -->
+      <!-- <textarea></textarea>
+      <select></select>
+      <input type="checkbox">  -->
+      <p>{{month}}개월 선택 하셨습니다. 총 {{원룸들[clickNumber].price*month}}</p>
+
+      <!-- input - 입력했을때, chage - 입력후 다른 곳을 클릭시 -->
       <button @click="$emit('closeModal')"> 닫기</button>
     <!-- 자식은 모달창 = false 처럼 변경 불가능 -->
     </div>
@@ -15,6 +24,11 @@
 export default {
     //eslint-disable-next-line
     name : "Modal",
+    data(){
+      return{
+        month : 1,
+      }
+    },
     props : {
         // 변수명 : 타입
         원룸들 : Object,
